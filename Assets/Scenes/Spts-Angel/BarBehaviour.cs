8212a19d901bb15046d8bb4d2489aff6eaaa3e2a
Angel CC <angelcenerini@gmail.com>
2022-09-21 13:46:38 -0400

    Angel: Mechanics

---
+++ Assets/Scenes/Spts-Angel/BarBehaviour.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarBehaviour : MonoBehaviour
+{
+    // Variables 
+    float currentHealth;
+    public float fillValue = 1f;
+    float declineSpeed;
+    public Image fillImage;
+    private Slider slider;
+
+    // Access Character Collisions
+    public CharacterBehaviour bS;
+
+
+    private void Awake()
+    {
+        // Fetch Slider
+        slider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Set Thirst(Damage) Speed & Bar Color
+        // Check Bar Status
+        if (slider.value >= 3 * slider.maxValue / 4)
+        {
+            // Slow Damage
+            declineSpeed = 0.0001f;
+            // Bar Color
+            fillImage.color = new Color(210f / 255f, 52f / 255f, 32f / 255f);
+        }
+        else if (slider.value <= 3 * slider.maxValue / 4 && slider.value >= slider.maxValue / 4)
+        {
+            // Medium Damage
+            declineSpeed = 0.00015f;
+            // Bar Color
+            fillImage.color = new Color(138f / 255f, 30f / 255f, 56f / 255f);
+        }
+        else if (slider.value <= slider.maxValue / 4)
+        {
+            // Quick Damage
+            declineSpeed = 0.0002f;
+            // Bar Color
+            fillImage.color = new Color(65f / 255f, 27f / 255f, 80f / 255f);
+        }
+
+
+        // Update Bar Value
+        // Continuos Thirst Decline
+        fillValue -= declineSpeed;
+        // Assign Value to Slider
+        slider.value = fillValue;
+
+        // Adjust Min Value
+        if (slider.value <= slider.minValue)
+        {
+            // Disable Bar Fill
+            fillImage.enabled = false;
+        }
+
+
+        // Apply Collision Repercussions
+        // Check Character Collision
+        if (bS.isCollided)
+        {
+            Debug.Log(bS.isCollided);
+            // React to Collision
+            fillValue += bS.collisionResult;
+            // Continuos Thirst Decline
+            fillValue -= declineSpeed;
+            // Assign Value to Slider
+            slider.value = fillValue;
+
+        }
+        else
+        {
+            Debug.Log(bS.isCollided);
+            // Continuos Thirst Decline
+            fillValue -= declineSpeed;
+            // Assign Value to Slider
+            slider.value = fillValue;
+        }
+    }
+}
---
+++ Assets/Scenes/Spts-Angel/CharacterBehaviour.cs
@@ -0,0 +1,163 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBehaviour : MonoBehaviour
+{
+    // Variables
+    // Movement
+    Rigidbody2D m_Rigidbody;
+    public bool isThrown;
+    public bool isMoving;
+    public bool isSpeeding;
+
+    // Rotation
+    float rotZ;
+    public float RotationSpeed = 100f;
+
+    // Accel. & Decel.
+    public float maxSpeed = 6f;
+    public float timeZeroToMax = 2.5f;
+    public float timeMaxToZero = 3f;
+    float accelRatePerSec;
+    float decelRatePerSec;
+    float forwardVelocity;
+
+    // Collision
+    public bool isCollided;
+    public float collisionResult;
+    public float TimeLeft = 1f;
+    public bool TimerOn = false;
+
+    void Start()
+    {
+        // Fetch Rigidbody
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+
+        // Calculate Acceleration
+        accelRatePerSec = maxSpeed / timeZeroToMax;
+
+        // Calculate Deceleration
+        decelRatePerSec = -maxSpeed / timeMaxToZero;
+
+        // Set Initial Velocity
+        forwardVelocity = 0f;
+
+        // Set Initial Status
+        isThrown = false;
+
+        // Set Initial Collision Result
+        isCollided = false;
+        collisionResult = 0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ROTATION MECHANIC
+        // Switch Rotation ON/OFF
+        // Check Velocity
+        if (forwardVelocity <= 0)
+        {
+            isThrown = false;
+        }
+        else
+        {
+            isThrown = true;
+        }
+        // Rotation
+        // Check Status
+        if (!isThrown)
+        {
+            // Rotate 
+            rotZ += Time.deltaTime * RotationSpeed;
+            transform.rotation = Quaternion.Euler(0, 0, rotZ);
+        }
+
+
+        // LAUNCHING MECHANIC
+        // Check Status
+        if (forwardVelocity <= 0)
+        {
+            // Register Input
+            if (Input.GetKey(KeyCode.Space))
+            {
+                // Launch
+                isMoving = true;
+                isSpeeding = true;
+            }
+        }
+        // Move
+        if (isMoving)
+        {
+            // Move Forward
+            m_Rigidbody.velocity = transform.up * forwardVelocity;
+
+            // Check Status
+            // Accelerating
+            if (isSpeeding)
+            {
+                // Start Accelerating
+                forwardVelocity += accelRatePerSec * Time.deltaTime;
+                forwardVelocity = Mathf.Min(forwardVelocity, maxSpeed);
+            }
+            // Decelerating
+            else
+            {
+                // Start Decelerating
+                forwardVelocity += decelRatePerSec * Time.deltaTime;
+                forwardVelocity = Mathf.Max(forwardVelocity, 0);
+            }
+
+            // Start Deceleratin & Reset Sprint
+            if (forwardVelocity >= maxSpeed)
+            {
+                // Stop Speeding
+                isSpeeding = false;
+            }
+        }
+
+
+        // COLLISION TIMER
+        if (TimerOn)
+        {
+            if (TimeLeft > 0)
+            {
+                TimeLeft -= Time.deltaTime;
+            }
+            else if(TimeLeft <= 0)
+            {
+                Debug.Log("Time is UP!");
+                isCollided = false;
+                TimerOn = false;
+                TimeLeft = 1f;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        TimerOn = true;
+
+        // Set Collision Reaction
+        if (collision.gameObject.tag == "BigBoss")
+        {
+            // Recognize Collision
+            isCollided = true;
+            collisionResult = 0.1f;
+        }
+        else if (collision.gameObject.tag == "MediumTarget")
+        {
+            // Recognize Collision
+            isCollided = true;
+            collisionResult = 0.2f;
+        }
+        else if (collision.gameObject.tag == "Attacker")
+        {
+            // Recognize Collision
+            isCollided = true;
+            collisionResult = -0.2f;
+        }
+    }
+}
